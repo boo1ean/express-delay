1340ce2052f3a91e136e7c03959b2dcf52a8a9a9
Daniel Stefanovic <daniel.stefanovic@gmail.com>
2015-07-08 13:16:46 +0200

    Add random delay

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var timers = require('timers');
 
-var create_delayer = function(time) {
+var get_random_int = function(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var create_delayer = function(min, max) {
+    var useRandomDelay = (arguments.length === 2);
+
 	return function(req, res, next) {
 		var send = res.send;
+		var time = useRandomDelay ? get_random_int(min, max) : min;
 
 		res.send = function() {
 			var args = arguments;
